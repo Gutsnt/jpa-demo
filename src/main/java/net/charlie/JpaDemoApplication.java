c46a3165ae0525641d13e63b8cb0b94438138430
Gutsnt <charlie.mendoza1337@gmail.com>
2022-05-11 21:00:29 -0400

    Repository

--- src/main/java/net/charlie/JpaDemoApplication.java
+++ src/main/java/net/charlie/JpaDemoApplication.java
@@ -1,20 +1,26 @@
 package net.charlie;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import net.charlie.repository.CategoriasRepository;
+
 @SpringBootApplication
 public class JpaDemoApplication implements CommandLineRunner {
-
+	
+	@Autowired
+	private CategoriasRepository repo;
+	//NO TOCAR
 	public static void main(String[] args) {
-		SpringApplication.run(JpaDemoApplication.class, args);
+		SpringApplication.run(JpaDemoApplication.class, args);	
+	
 	}
-
+	
 	@Override
 	public void run(String... args) throws Exception {
-		
-		
+		System.out.println(repo);
 	}
 
 }
---
+++ src/main/java/net/charlie/repository/CategoriasRepository.java
@@ -0,0 +1,9 @@
+package net.charlie.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import net.charlie.model.Categoria;
+
+public interface CategoriasRepository extends CrudRepository<Categoria, Integer> {
+
+}
